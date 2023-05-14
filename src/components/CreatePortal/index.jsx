ccbadda585f383a2ce8367af622f2ba3d3cedc13
smartwen <smartwen@tencent.com>
2023-05-14 13:23:52 +0800

    feat: use hooks

--- src/components/CreatePortal/index.jsx
+++ src/components/CreatePortal/index.jsx
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const containerRef = React.createRef();
-//ReactDOM.createPortal不支持ref操作
-class CreatePortalDialog extends Component {
-    handleClick = () => {
+// const containerRef = React.createRef();
+// //ReactDOM.createPortal不支持ref操作
+// class CreatePortalDialog extends Component {
+//     handleClick = () => {
+//         // ReactDOM.unmountComponentAtNode(this.ref);
+//     }
+//     render() {
+//         return ReactDOM.createPortal(
+//             <div className="mask" onClick={this.handleClick}>
+//                 {this.props.children}
+//             </div>,
+//             document.body
+//         )
+//     }
+// }
+
+function CreatePortalDialog({children}) {
+    const handleClick = () => {
         // ReactDOM.unmountComponentAtNode(this.ref);
     }
-    render() {
-        return ReactDOM.createPortal(
-            <div className="mask" onClick={this.handleClick} ref={containerRef}>
-                {this.props.children}
-            </div>,
-            document.body
-        )
-    }
+
+    return ReactDOM.createPortal(
+        <div className="mask" onClick={handleClick}>
+            {children}
+        </div>,
+        document.body
+    )
 }
+
 
 export default CreatePortalDialog;
 
